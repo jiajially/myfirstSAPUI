02871fab6335c912dd94ef682b4321c5840fbc7f
jiajially <445178847@qq.com>
2016-12-09 11:00:21 +0800

    FISRT

---
+++ WebContent/myfirstsapui5/test.view.js
@@ -0,0 +1,24 @@
+sap.ui.jsview("myfirstsapui5.test", {
+
+	/** Specifies the Controller belonging to this View. 
+	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
+	* @memberOf myfirstsapui5.test
+	*/ 
+	getControllerName : function() {
+		return "myfirstsapui5.test";
+	},
+
+	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
+	* Since the Controller is given to this method, its event handlers can be attached right away. 
+	* @memberOf myfirstsapui5.test
+	*/ 
+	createContent : function(oController) {
+ 		return new sap.m.Page({
+			title: "Title",
+			content: [
+			
+			]
+		});
+	}
+
+});
